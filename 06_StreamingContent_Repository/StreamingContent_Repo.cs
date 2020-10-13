c7c7ecd4179eb1c35db0ac325ce924277a1df98d
Michael Pabody <michael.pabody@gmail.com>
2020-10-13 15:53:51 -0400

    finished repo methods and repo tests

---
+++ 06_StreamingContent_Repository/StreamingContent_Repo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_StreamingContent_Repository
+{
+    public class StreamingContent_Repo
+    {
+        private List<StreamingContent> _contentDirectory = new List<StreamingContent>();
+
+        public bool AddContentToDirectory(StreamingContent content)
+        {
+            int startingCount = _contentDirectory.Count;
+
+            _contentDirectory.Add(content);
+
+            bool wasAdded = (_contentDirectory.Count > startingCount) ? true : false;
+            return wasAdded;
+        }
+
+        public List<StreamingContent> GetContents()
+        {
+            return _contentDirectory;
+        }
+
+        public StreamingContent GetContentByTitle(string title)
+        {
+            foreach (StreamingContent content in _contentDirectory)
+            {
+                if(content.Title.ToLower() == title.ToLower())
+                {
+                    return content;
+                }
+            }
+            return null;
+        }
+
+        public bool UpdateExistingContent(string originalTitle, StreamingContent newContent)
+        {
+            StreamingContent oldContent = GetContentByTitle(originalTitle);
+
+            if (oldContent != null)
+            {
+                oldContent.Title = newContent.Title;
+                oldContent.Description = newContent.Description;
+                oldContent.StarRating = newContent.StarRating;
+                oldContent.MaturityRating = newContent.MaturityRating;
+                oldContent.Genre = newContent.Genre;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteExistingContent(StreamingContent existingContent)
+        {
+            bool deleteResult = _contentDirectory.Remove(existingContent);
+            return deleteResult;
+        }
+    }
+}
---
+++ 06_StreamingContent_Tests/StreamingContent_RepoTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _06_StreamingContent_Repository;
+
+namespace _06_StreamingContent_Tests
+{
+    [TestClass]
+    public class StreamingContent_RepoTests
+    {
+        
+        [TestMethod]
+        public void AddToDirectory_ShouldGetCorrectBoolean()
+        {
+            //Arrange
+            StreamingContent content = new StreamingContent();
+            StreamingContent_Repo repository = new StreamingContent_Repo();
+
+            //Act
+            bool addResult = repository.AddContentToDirectory(content);
+
+            //Assert
+            Assert.IsTrue(addResult);
+        }
+
+        [TestMethod]
+        public void GetDirectory_ShouldReturnCorrectCollection()
+        {
+            //Arrange
+            StreamingContent content = new StreamingContent();
+            StreamingContent_Repo repo = new StreamingContent_Repo();
+
+            repo.AddContentToDirectory(content);
+
+            //Act
+            List<StreamingContent> contents = repo.GetContents();
+
+            bool directoryHasContent = contents.Contains(content);
+
+            //Assert
+            Assert.IsTrue(directoryHasContent);
+        }
+
+        [TestMethod]
+        public void GetByTitle_ShouldReturnCorrectContent()
+        {
+            //Arrange
+            StreamingContent_Repo repo = new StreamingContent_Repo();
+            StreamingContent newContent = new StreamingContent("Toy Story", "Toys come to life", 10, Genre.Action, MaturityRating.PG);
+            repo.AddContentToDirectory(newContent);
+            string title = "Toy Story";
+            
+            //Act
+            StreamingContent searchResult = repo.GetContentByTitle(title);
+            //Assert
+            Assert.AreEqual(searchResult.Title, title);
+        }
+
+        [TestMethod]
+        public void UpdateExistingContent_ShouldReturnTrue()
+        {
+            //Arrange
+            StreamingContent_Repo repo = new StreamingContent_Repo();
+            StreamingContent oldContent = new StreamingContent("Toy Story", "Toys come to life", 8, Genre.Drama, MaturityRating.PG);
+            repo.AddContentToDirectory(oldContent);
+
+            StreamingContent newContent = new StreamingContent("Toy Story", "Toys come to life", 10, Genre.Action, MaturityRating.PG);
+
+            //Act
+            bool updateResult = repo.UpdateExistingContent(oldContent.Title, newContent);
+
+            //Assert
+            Assert.IsTrue(updateResult);
+        }
+
+        [TestMethod]
+        public void DeleteExistingContent_ShouldReturnTrue()
+        {
+            //Arrange
+            StreamingContent_Repo repo = new StreamingContent_Repo();
+            StreamingContent content = new StreamingContent("Toy Story", "Toys come to life", 8, Genre.Drama, MaturityRating.PG);
+            repo.AddContentToDirectory(content);
+
+            //Act
+            StreamingContent oldContent = repo.GetContentByTitle("Toy Story");
+
+            bool removeResult = repo.DeleteExistingContent(oldContent);
+
+            //Assert
+            Assert.IsTrue(removeResult);
+        }
+    }
+}
